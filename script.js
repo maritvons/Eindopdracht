d40cc9210ef6dfba77213995a348841dea144533
Marit Vons <74767400+maritvons@users.noreply.github.com>
2023-04-06 13:12:29 +0000

    obstakels

--- script.js
+++ script.js
@@ -3,11 +3,20 @@ var bgImg;
 
 var mario;
 var mario_run, mario_jump, mario_gone;
+var ob1, ob2, ob3, ob4;
+
+var obG;
 
 var score = 0;
 
 function preload() {
+  ob1 = loadImage("Img/ob1.png");
+  ob2 = loadImage("Img/ob2.png");
+  ob3 = loadImage("Img/ob3.png");
+  ob4 = loadImage("Img/ob4.png");
+  
   bgImg = loadImage("Img/bg.png");
+  
   mario_run = loadAnimation("Img/run1.png", "Img/run2.png", "Img/run3.png");
   mario_jump = loadImage("Img/jump.png");
   mario_gone = loadAnimation("Img/end.png");
@@ -22,6 +31,8 @@ function setup() {
   ground.visible = false
 
   mario = createSprite(60, 450, 10, 10);
+
+  obG = new Group();
 
   back.addImage(bgImg)
   back.scale = 1.575
@@ -54,8 +65,41 @@ function draw() {
     mario.velocityY = -15
   }
   back.velocityX = -(3.7 + score / 100)
+  if (frameCount % 150 === 0) {
+      Obstacle();
+    }
 
   fill(0)
   textSize(20)
   text("Score=" + score, 30, 45)
+}
+
+function Obstacle() {
+  ob = createSprite(850, 433, 10, 10)
+  ob.depth = mario.depth - 1
+  ob.lifetime = 700
+  var obrand = Math.round(random(1, 4))
+  if (obrand === 1) {
+    ob.addImage(ob1)
+    ob.scale = 0.5
+    ob.setCollider("rectangle", 0, 0, 100, 100)
+  } else if (obrand === 2) {
+    ob.addImage(ob2)
+    ob.y = ground.y - 75
+    ob.scale = 0.5
+    ob.setCollider("rectangle", 0, 0, 100, 200)
+  } else if (obrand === 3) {
+    ob.addImage(ob3)
+    ob.scale = 0.5
+    ob.setCollider("rectangle", 0, 0, 200, 200)
+
+  } else if (obrand === 4) {
+    ob.addImage(ob4)
+    ob.y = 470
+    ob.scale = 0.5
+  }
+  ob.velocityX = -(4 + score / 100)
+  obG.add(ob)
+  ob.debug = false
+  ob.lifetime = 500
 }
